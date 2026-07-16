d550d7bd9d167b3afe775edfe7228551f8a22cf6
Yuzuru Ichihara <guzuruyuzuru@gmail.com>
2026-07-16 22:18:31 +0900

    anime.jsのホバーアニメーションを追加

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
 import './style.css';
+import { animate } from 'animejs';
 
-alert('app.js');
+const button = document.querySelector('.anime-btn');
+
+button.addEventListener('mouseenter', () => {
+  animate('.anime-btn', {
+    scale: 1.2,
+    rotate: '5deg',
+    duration: 800,
+    ease: 'outElastic',
+  });
+});
+
+button.addEventListener('mouseleave', () => {
+  animate('.anime-btn', {
+    scale: 1,
+    rotate: '0deg',
+    duration: 500,
+    ease: 'outQuint',
+  });
+});
